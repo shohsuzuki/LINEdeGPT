a1d59979e32d94ac7b98202088f79d3dc1d53112
suzuki sho <suzukisho@suzukinoMacBook-Pro.local>
2025-05-11 21:02:39 +0900

    fix: remove full-width space

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-　　secret = os.getenv("LINE_CHANNEL_SECRET")
+    secret = os.getenv("LINE_CHANNEL_SECRET")
     reply = f"SECRET is: {secret}"
 
     line_bot_api.reply_message(
